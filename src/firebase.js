6f8a747f0c1c012ac635c00716955a81ffad0ef4
gwagsi <gwagsiglenn@gmail.com>
2020-09-11 15:22:09 +0100

    create react app and set header layout

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCKl148QwR0JHtF_QgS4lQRW9JCeOq9A4o",
+  authDomain: "clone-1fcf3.firebaseapp.com",
+  databaseURL: "https://clone-1fcf3.firebaseio.com",
+  projectId: "clone-1fcf3",
+  storageBucket: "clone-1fcf3.appspot.com",
+  messagingSenderId: "666431428139",
+  appId: "1:666431428139:web:ec0b6463c21a32f3153c6e",
+  measurementId: "G-TGCNL43KM3",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
